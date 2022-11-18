09b4aeb8c47ed81a2a674c07165f521760e96529
Matan <Matan1111@gmail.com>
2022-11-18 15:17:38 +0200

    changed main to match output format

--- main.c
+++ main.c
@@ -5,17 +5,8 @@ int main()
 {
     int n1, n2;
     printf("Enter two numbers: ");
-    scanf("%d", &n1);
-    scanf("%d", &n2);
-    printf("Prime numbers: ");
-    for (int i = n1; i <= n2; i++)
-    {
-        if (isPrime(i))
-        {
-            printf("%d ", i);
-        }
-    }
-    printf("\nArmstrong numbers: ");
+    scanf("%d %d", &n1, &n2);
+    printf("\nThe Armstrong numbers are: ");
     for (int i = n1; i <= n2; i++)
     {
         if (isArmstrong(i))
@@ -23,15 +14,7 @@ int main()
             printf("%d ", i);
         }
     }
-    printf("\nStrong numbers: ");
-    for (int i = n1; i <= n2; i++)
-    {
-        if (isStrong(i))
-        {
-            printf("%d ", i);
-        }
-    }
-    printf("\nPalindrome numbers: ");
+    printf("\nThe Palindromes are: ");
     for (int i = n1; i <= n2; i++)
     {
         if (isPalindrome(i))
@@ -39,5 +22,21 @@ int main()
             printf("%d ", i);
         }
     }
-    printf("\n");
+    printf("\nThe Prime numbers are: ");
+    for (int i = n1; i <= n2; i++)
+    {
+        if (isPrime(i))
+        {
+            printf("%d ", i);
+        }
+    }
+    printf("\nThe Strong numbers are: ");
+    for (int i = n1; i <= n2; i++)
+    {
+        if (isStrong(i))
+        {
+            printf("%d ", i);
+        }
+    }
+    return 0;
 }
