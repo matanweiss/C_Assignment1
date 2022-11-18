b71fef74ff8eaf05aac085905488912bf595ad02
Matan <Matan1111@gmail.com>
2022-11-18 15:24:23 +0200

    updated main to match output format 2

--- main.c
+++ main.c
@@ -4,38 +4,37 @@
 int main()
 {
     int n1, n2;
-    printf("Enter two numbers: ");
     scanf("%d %d", &n1, &n2);
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for (int i = n1; i <= n2; i++)
     {
         if (isArmstrong(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nThe Palindromes are: ");
+    printf("\nThe Palindromes are:");
     for (int i = n1; i <= n2; i++)
     {
         if (isPalindrome(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nThe Prime numbers are: ");
+    printf("\nThe Prime numbers are:");
     for (int i = n1; i <= n2; i++)
     {
         if (isPrime(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\nThe Strong numbers are: ");
+    printf("\nThe Strong numbers are:");
     for (int i = n1; i <= n2; i++)
     {
         if (isStrong(i))
         {
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
     return 0;
